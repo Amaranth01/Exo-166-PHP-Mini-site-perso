7fd9be78edf5fbcbad7e14e612cf532c03a8a800
Amaranth01 <vanessa.nectoux@gmail.com>
2022-01-26 16:01:20 +0100

    étape 6 ou plus

--- parts/menu.php
+++ parts/menu.php
@@ -1,8 +1,8 @@
 <nav
     <ul>
-        <li><a href="../public/?page=bio">Bio</a></li>
-        <li><a href="../public/?page=contact">Contact</a></li>
-        <li><a href="../public/">Home Page</a></li>
+        <li><a href="../public?page=bio">Bio</a></li>
+        <li><a href="../public?page=contact">Contact</a></li>
+        <li><a href="../public">Home Page</a></li>
     </ul>
 
 </nav>
